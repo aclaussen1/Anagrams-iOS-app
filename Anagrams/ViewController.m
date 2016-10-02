6cc222392663681d930e42cbc19f5606a8b289b7
Alex <aclaussen@ymail.com>
2016-10-02 19:05:18 -0400

    create onscreen view

--- Anagrams/ViewController.m
+++ Anagrams/ViewController.m
@@ -29,6 +29,12 @@
     [super viewDidLoad];
     Level* level1 = [Level levelWithNum:1];
     NSLog(@"anagrams: %@", level1.anagrams);
+    
+    //add one layer for all game elements. Create a new view iwth dimensions of the screen. Assign it to the gameView property of the Game controller instance. The GameController instance will use this view for all game elements
+    UIView* gameLayer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
+    [self.view addSubview:gameLayer];
+    
+    self.controller.gameView = gameLayer;
 }
 
 @end
