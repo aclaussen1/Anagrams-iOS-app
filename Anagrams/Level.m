09b621ddebe36b4fb065cc8432954409c488e821
Alex <aclaussen@ymail.com>
2016-10-02 18:40:48 -0400

    +(instancetype)levelWithNum:(int)levelNum in Level class implementation to create Level instances

--- Anagrams/Level.m
+++ Anagrams/Level.m
@@ -10,4 +10,27 @@
 
 @implementation Level
 
++(instancetype)levelWithNum:(int)levelNum;
+{
+    //find .plist file for this level
+    NSString* fileName = [NSString stringWithFormat:@"level%i.plist", levelNum];
+    NSString* levelPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:fileName];
+    
+    //load .plist file
+    NSDictionary* levelDict = [NSDictionary dictionaryWithContentsOfFile:levelPath];
+    
+    //validation
+    NSAssert(levelDict, @"level config file not found");
+    
+    //create level instance
+    Level* l = [[Level alloc] init];
+    
+    //initialize the object from the dictionary
+    l.pointsPerTile = [levelDict[@"pointsPerTile"] intValue];
+    l.anagrams = levelDict[@"anagrams"];
+    l.timeToSolve = [levelDict[@"timeToSolve"] intValue];
+    
+    return l;
+}
+
 @end
